5988e00081f3bbca81238e210fb33c32fabfe25f
Gosha924 <151758180+Gosha924@users.noreply.github.com>
2026-04-01 17:41:09 +0300

    add linked list and tests

--- src/main/kotlin/CustomList.kt
+++ src/main/kotlin/CustomList.kt
@@ -2,10 +2,11 @@ package org.example
 
 interface CustomList : Iterable<Int> {
     operator fun get(index: Int): Int
-    operator fun set(index: Int, value: Int)
+    operator fun set(index: Int, value: Int) :Int
     fun add(element: Int)
     fun addFirst(element: Int)
     fun remove(element: Int): Boolean
     fun indexOf(element: Int): Int
+    fun isEmpty(): Boolean
     val size: Int
 }
--- src/test/kotlin/SingleLinkedListTest.kt
+++ src/test/kotlin/SingleLinkedListTest.kt
@@ -101,4 +101,34 @@ class SingleLinkedListTest {
         list[2] = 5
         assertEquals(5, list[2])
     }
+    @Test
+    fun `remove duplicate elements removes only first occurrence`() {
+        list.add(1)
+        list.add(2)
+        list.add(1)
+        list.remove(1)
+        assertEquals(2, list.size)
+        assertEquals(2, list[0])
+        assertEquals(1, list[1])
+    }
+
+    @Test
+    fun `indexOf returns -1 if element not found`() {
+        list.add(1)
+        list.add(2)
+        assertEquals(-1, list.indexOf(123456789))
+    }
+
+    @Test
+    fun `isEmpty returns true for empty list`() {
+        assertTrue(list.isEmpty())
+    }
+
+    @Test
+    fun `isEmpty returns false after add`() {
+        list.add(1)
+        assertFalse(list.isEmpty())
+    }
+
+
 }
